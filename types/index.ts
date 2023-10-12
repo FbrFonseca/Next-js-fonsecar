178b12d11f03be7b2f546689b69aea2fefc64c63
Fabricio Pereira Fonseca <f.pereirafonseca@gmail.com>
2023-10-12 16:53:43 +0100

    filters completed

--- types/index.ts
+++ types/index.ts
@@ -1,3 +1,4 @@
+import exp from "constants";
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
@@ -32,9 +33,19 @@ export interface CarProps {
 }
 
 export interface FilterProps {
-    manufacturer: string,
-    year: number,
-    fuel: string,
-    limit: number,
-    model: string,
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
 }
